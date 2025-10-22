ebdb30e79c2c78a15bf83b845260603bba2f9579
0x4nud33p <anudeepavula009@gmail.com>
2025-10-22 21:51:18 +0530

    feat:withdraw and swap inst

--- anchor-amm-starter-q4-25/programs/anchor-amm-q4-25/src/instructions/swap.rs
+++ anchor-amm-starter-q4-25/programs/anchor-amm-q4-25/src/instructions/swap.rs
@@ -7,20 +7,135 @@ use constant_product_curve::{ConstantProduct, LiquidityPair};
 
 use crate::{errors::AmmError, state::Config};
 
-// #[derive(Accounts)]
-// pub struct Swap<'info> {
-// TODO: Write the accounts struct
-// }
+#[derive(Accounts)]
+pub struct Swap<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
 
-// impl<'info> Swap<'info> {
-//     pub fn swap(&mut self, is_x: bool, amount: u64, min: u64) -> Result<()> {
-// TODO
-//}
+    pub mint_x: Account<'info, Mint>,
+    pub mint_y: Account<'info, Mint>,
 
-//     pub fn deposit_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
-// TODO
-//}
+    #[account(
+        mut,
+        associated_token::mint = mint_x,
+        associated_token::authority = user,
+    )]
+    pub user_x: Account<'info, TokenAccount>,
 
-//     pub fn withdraw_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {}
-// TODO
-//}
+    #[account(
+        mut,
+        associated_token::mint = mint_y,
+        associated_token::authority = user,
+    )]
+    pub user_y: Account<'info, TokenAccount>,
+
+    #[account(
+        has_one = mint_x,
+        has_one = mint_y,
+        seeds = [b"config", config.seed.to_le_bytes().as_ref()],
+        bump = config.config_bump,
+    )]
+    pub config: Account<'info, Config>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_x,
+        associated_token::authority = config,
+    )]
+    pub vault_x: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_y,
+        associated_token::authority = config,
+    )]
+    pub vault_y: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        seeds = [b"lp", config.key().as_ref()],
+        bump = config.lp_bump,
+    )]
+    pub mint_lp: Account<'info, Mint>,
+
+    #[account(
+        init_if_needed,
+        payer = user,
+        associated_token::mint = mint_lp,
+        associated_token::authority = user,
+    )]
+    pub user_lp: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+impl<'info> Swap<'info> {
+    pub fn swap(&mut self, is_x: bool, amount: u64, min: u64) -> Result<()> {
+        require!(amount > 0, AmmError::InvalidAmount);
+
+        let mut curve = ConstantProduct::init(
+            self.vault_x.amount,
+            self.vault_y.amount,
+            self.mint_lp.supply,
+            self.config.fee,
+            Some(6),
+        )
+        .unwrap();
+
+        let pair = if is_x {
+            LiquidityPair::X
+        } else {
+            LiquidityPair::Y
+        };
+
+        let swap_result = curve.swap(pair, amount, min).unwrap();
+
+        self.deposit_tokens(is_x, swap_result.deposit)?;
+        self.withdraw_tokens(!is_x, swap_result.withdraw)?;
+
+        Ok(())
+    }
+
+    pub fn deposit_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
+        let (from, to) = if is_x {
+            (self.user_x.to_account_info(), self.vault_x.to_account_info())
+        } else {
+            (self.user_y.to_account_info(), self.vault_y.to_account_info())
+        };
+
+        let cpi_accounts = Transfer {
+            from,
+            to,
+            authority: self.user.to_account_info(),
+        };
+        let ctx = CpiContext::new(self.token_program.to_account_info(), cpi_accounts);
+
+        transfer(ctx, amount)
+    }
+
+    pub fn withdraw_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
+        let (from, to) = if is_x {
+            (self.vault_x.to_account_info(), self.user_x.to_account_info())
+        } else {
+            (self.vault_y.to_account_info(), self.user_y.to_account_info())
+        };
+
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"config",
+            &self.config.seed.to_le_bytes(),
+            &[self.config.config_bump],
+        ]];
+
+        let cpi_accounts = Transfer {
+            from,
+            to,
+            authority: self.config.to_account_info(),
+        };
+        let ctx =
+            CpiContext::new_with_signer(self.token_program.to_account_info(), cpi_accounts, signer_seeds);
+
+        transfer(ctx, amount)
+    }
+}
